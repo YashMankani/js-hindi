7d0a924eebdf8393334bf28da4de53bcae74689d
Swastik Mankani <119824766+YashMankani@users.noreply.github.com>
2024-03-19 15:08:36 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,84 @@
+// let score = "33abc"
+
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber) // number but actually it is not
+// console.log(valueInNumber) // NaN (Not a Number)
+
+
+
+// score = null
+
+// console.log(typeof(score));
+
+// valueInNumber = Number(score)
+// console.log(typeof valueInNumber) // number but actually it is not
+// console.log(valueInNumber) // 0
+
+
+
+// score = undefined
+
+// console.log(typeof(score));
+
+// valueInNumber = Number(score)
+// console.log(typeof valueInNumber) // number but actually it is not
+// console.log(valueInNumber) // NaN (Not a Number)
+
+
+
+// score = true
+
+// console.log(typeof(score));
+
+// valueInNumber = Number(score)
+// console.log(typeof valueInNumber) // number but actually it is not
+// console.log(valueInNumber) // 1
+
+
+
+// score = false
+
+// console.log(typeof(score));
+
+// valueInNumber = Number(score)
+// console.log(typeof valueInNumber) // number but actually it is not
+// console.log(valueInNumber) // 0
+
+// Notes:
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// undefined => NaN
+// true => 1
+// false => 0
+
+// let isLoggedIn = 1
+
+// let isLoggedOut = 0
+
+// let booleanLoggedIn = Boolean(isLoggedIn)
+// let booleanIsLoggedOut = Boolean(isLoggedOut)
+
+// console.log(booleanLoggedIn);
+// console.log(booleanIsLoggedOut);
+
+
+// isLoggedIn = "Swastik"
+
+// isLoggedOut = ""
+
+// booleanLoggedIn = Boolean(isLoggedIn)
+// booleanIsLoggedOut = Boolean(isLoggedOut)
+
+// console.log(booleanLoggedIn);
+// console.log(booleanIsLoggedOut);
+
+// "Swastik" => true
+// "" => false
+
+let someMumber = 33
+let stringNumber = String(someMumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
