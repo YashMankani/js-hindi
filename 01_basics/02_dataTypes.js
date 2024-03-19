c252922143014934b9750f075591d0c6df4da362
Swastik Mankani <119824766+YashMankani@users.noreply.github.com>
2024-03-19 12:09:17 +0000

    config

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";//treat all JS code as newer version.
+
+// alert("hello") // we are using nodejs, not a browser..
+
+console.log(3
+    +
+    3) // code readibility should be high
+
+console.log("Swastik");
+
+
+let name = "Swastik" // string
+let age = 14 // numbers
+let isLoggedIn = true // Boolian
+let blog = null // null
+let state; // undefined
+
+
+// range of numbers is 2 to the power of 53
+// bigint
+// string => "" recommended
+// boolean => true/false
+// null => standalone value in js, we can define as a null which means empty
+// undefined => not defined or assigned
+
+// object
+
+console.log();
