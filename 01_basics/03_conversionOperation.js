1067b76c24989ac47ba6c571540fb628f12f85a3
Swastik Mankani <119824766+YashMankani@users.noreply.github.com>
2024-03-20 02:29:25 +0000

    conversion operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,48 +1,48 @@
-// let score = "33abc"
+let score = "33abc"
 
 // console.log(typeof(score));
 
-// let valueInNumber = Number(score)
+let valueInNumber = Number(score)
 // console.log(typeof valueInNumber) // number but actually it is not
 // console.log(valueInNumber) // NaN (Not a Number)
 
 
 
-// score = null
+score = null
 
 // console.log(typeof(score));
 
-// valueInNumber = Number(score)
+valueInNumber = Number(score)
 // console.log(typeof valueInNumber) // number but actually it is not
 // console.log(valueInNumber) // 0
 
 
 
-// score = undefined
+score = undefined
 
 // console.log(typeof(score));
 
-// valueInNumber = Number(score)
+valueInNumber = Number(score)
 // console.log(typeof valueInNumber) // number but actually it is not
 // console.log(valueInNumber) // NaN (Not a Number)
 
 
 
-// score = true
+score = true
 
 // console.log(typeof(score));
 
-// valueInNumber = Number(score)
+valueInNumber = Number(score)
 // console.log(typeof valueInNumber) // number but actually it is not
 // console.log(valueInNumber) // 1
 
 
 
-// score = false
+score = false
 
 // console.log(typeof(score));
 
-// valueInNumber = Number(score)
+valueInNumber = Number(score)
 // console.log(typeof valueInNumber) // number but actually it is not
 // console.log(valueInNumber) // 0
 
@@ -54,23 +54,23 @@
 // true => 1
 // false => 0
 
-// let isLoggedIn = 1
+let isLoggedIn = 1
 
-// let isLoggedOut = 0
+let isLoggedOut = 0
 
-// let booleanLoggedIn = Boolean(isLoggedIn)
-// let booleanIsLoggedOut = Boolean(isLoggedOut)
+let booleanLoggedIn = Boolean(isLoggedIn)
+let booleanIsLoggedOut = Boolean(isLoggedOut)
 
 // console.log(booleanLoggedIn);
 // console.log(booleanIsLoggedOut);
 
 
-// isLoggedIn = "Swastik"
+isLoggedIn = "Swastik"
 
-// isLoggedOut = ""
+isLoggedOut = ""
 
-// booleanLoggedIn = Boolean(isLoggedIn)
-// booleanIsLoggedOut = Boolean(isLoggedOut)
+booleanLoggedIn = Boolean(isLoggedIn)
+booleanIsLoggedOut = Boolean(isLoggedOut)
 
 // console.log(booleanLoggedIn);
 // console.log(booleanIsLoggedOut);
@@ -79,6 +79,41 @@
 // "" => false
 
 let someMumber = 33
+
 let stringNumber = String(someMumber)
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+// *****************Operation*****************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2**3); //power
+// console.log(2/3); 
+// console.log(2%3); //reaminder
+
+let str1 = "Hello"
+let str2 = " Swastik"
+
+let product = str1 + str2
+
+// console.log(product);
+
+// console.log("2" + 1); // string
+// console.log(2 + "1"); // string
+// console.log("2" + 1 + 1); // string
+// console.log(2 + 1 + "1"); // number leaving lat string
+
+// console.log(1 + 2 * 3 % 4 / 5);
+
+// pls, pls, pls, use parenthesis..
+
+// console.log((1 + 2) * 3 % 4);
+
+// console.log(+true); 
+// console.log(+"");
